f08bba4f7128d7a1a3559394c3eaa8dd39495a3d
Geoff Garside <geoff@geoffgarside.co.uk>
2011-05-20 13:55:38 +0100

    Fix autoload reference for AsyncDispatch{ => er}

--- lib/xenapi.rb
+++ lib/xenapi.rb
@@ -1,6 +1,6 @@
 module XenApi #:nodoc:
-  autoload :Client,         File.expand_path('../xenapi/client',            __FILE__)
-  autoload :Errors,         File.expand_path('../xenapi/errors',            __FILE__)
-  autoload :Dispatcher,     File.expand_path('../xenapi/dispatcher',        __FILE__)
-  autoload :AsyncDispatch,  File.expand_path('../xenapi/async_dispatcher',  __FILE__)
+  autoload :Client,           File.expand_path('../xenapi/client',            __FILE__)
+  autoload :Errors,           File.expand_path('../xenapi/errors',            __FILE__)
+  autoload :Dispatcher,       File.expand_path('../xenapi/dispatcher',        __FILE__)
+  autoload :AsyncDispatcher,  File.expand_path('../xenapi/async_dispatcher',  __FILE__)
 end
